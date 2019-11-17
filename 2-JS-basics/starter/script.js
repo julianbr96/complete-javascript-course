31af56f9dde572baa3c75574d35a5804b28a67d6
julianbr96 <jbyr96@gmail.com>
2019-11-17 00:10:46 -0300

    added basic operators

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -24,7 +24,7 @@ console.log(job);
 /*
 * Variables and Data Types
 */
-
+/*
 var firstName = 'John';
 var age = 28;
 
@@ -47,3 +47,28 @@ alert(firstName + ' is a ' + age + ' years old '
 
 var lastName = prompt('What is his last name?');
 console.log(firstName + ' ' + lastName);
+*/
+
+/*
+* Basic Operators
+*/
+
+// Math Operators
+var year = 2019;
+var ageJohn = 28;
+var ageMark = 33;
+var yearJohn = year - ageJohn;
+var yearMark = year - ageMark;
+
+console.log(yearJohn);
+console.log(year + 2);
+console.log(year / 10);
+
+// Logical Operators
+console.log(ageJohn > ageMark);
+
+// typeof operator
+console.log(typeof ageJohn);
+console.log(typeof 'this');
+var x;
+console.log(typeof x);
