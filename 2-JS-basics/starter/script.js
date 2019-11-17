bd3f9196136412f41215b2ad94a45a54377b4c8f
julianbr96 <jbyr96@gmail.com>
2019-11-17 00:00:36 -0300

    added variable mutation

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -39,3 +39,11 @@ console.log(firstName + ' is a ' + age + ' years old '
 + job + '. Is he married? ' + isMarried);
 
 // Variable mutation 
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' years old ' 
++ job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
